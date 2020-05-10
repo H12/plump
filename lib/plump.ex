133c6f7590165ddfd31c599cf4c2148901e03a49
Henry Firth <henrynf12@gmail.com>
2020-05-10 10:41:15 -0500

    Add some basic functions to the API

--- lib/plump.ex
+++ lib/plump.ex
@@ -6,4 +6,22 @@ defmodule Plump do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias Plump.Boundary.{GameManager, GameSession}
+
+  def build_game(creator_name) do
+    GameManager.build_game(creator_name)
+  end
+
+  def list_games do
+    GameManager.list_games()
+  end
+
+  def add_player(code, player_name) do
+    GameManager.add_player(code, player_name)
+  end
+
+  def start_game(code) do
+    game = GameManager.lookup_game_by_code(code)
+    GameSession.start_game(game)
+  end
 end
