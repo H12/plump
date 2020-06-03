29809ef286f6777c2631f22db442ecadfcd945a8
Henry Firth <henrynf12@gmail.com>
2020-06-02 21:29:52 -0500

    Add a sessionless game view once a player joins

--- lib/plump_web/live/join_live.ex
+++ lib/plump_web/live/join_live.ex
@@ -3,18 +3,17 @@ defmodule PlumpWeb.JoinLive do
 
   @impl true
   def mount(%{"code" => code}, _session, socket) do
-    {:ok, assign(socket, code: code, player: "")}
+    {:ok, assign(socket, code: code, game: Plump.lookup_game_by_code(code))}
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, code: nil, player: "")}
+    {:ok, assign(socket, code: nil, game: nil)}
   end
 
   @impl true
   def handle_event("join", %{"code" => code, "player" => player}, socket) do
     {:ok, code} = Plump.add_player(code, player)
-
-    {:noreply, assign(socket, code: code, player: player)}
+    {:noreply, assign(socket, code: code, game: Plump.lookup_game_by_code(code))}
   end
 end
